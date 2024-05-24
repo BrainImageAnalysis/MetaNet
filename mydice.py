3b651ec903322d224153e2cdd287d7bd4356dd48
charissa <charissa.poon@riken.jp>
2024-05-24 12:19:18 +0900

    cleaned notebook

--- mydice.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-def mydice(seg, gt):
-    alpha = 0.00001
-    
-    dice_score = torch.sum(seg[gt==1.])*2. / (torch.sum(seg) + torch.sum(gt) + alpha)
-    
-    #print(dice_score)
-    return dice_score
